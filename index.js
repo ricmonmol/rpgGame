ed1da871f77ed6acdc855f4df007cf9cad3f434f
ricardo <rmontecinosmolina@gmail.com>
2022-02-28 13:56:09 -0300

    part two, new file

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
+import characterData from '/data.js'
 
-const hero = {
-    elementId : "hero",
-    name : "Wizard",
-    avatar : "images/wizard.png",
-    health : 60,
-    diceCount : 3
-}
-
-const monster = {
-    elementId : "monster",
-    name : "Orc",
-    avatar : "images/orc.png",
-    health : 10,
-    diceCount : 1
-}
-
-function renderCharacter(data){
-    const {elementId,name,avatar,health,diceCount} = data
-
-    const diceHtml = getDiceHtml(diceCount)
-
-    document.getElementById(elementId).innerHTML = '<div class="character-card"><h4 class="name">' + name + '</h4><img class="avatar" src=' + avatar + '/><p class="health">health: <b>' + health + '</b></p><div class="dice-container">' + diceHtml + '</div></div>'
+function Character(data){
+    Object.assign(this,data)
+    this.getDiceHtml = function(diceCount){
+        return getDiceRollArray(diceCount).map(function(num){
+            return '<div class="dice">' + num + '</div>'
+        }).join("")
+    }
+    this.getCharacterHtml = function(){
+        const {elementId,name,avatar,health,diceCount} = this
+        const diceHtml = this.getDiceHtml(diceCount)
+        return '<div class="character-card"><h4 class="name">' + name + '</h4><img class="avatar" src=' + avatar + '/><p class="health">health: <b>' + health + '</b></p><div class="dice-container">' + diceHtml + '</div></div>'
+    }
 }
 
 function getDiceRollArray(diceCount){
@@ -28,12 +19,12 @@ function getDiceRollArray(diceCount){
         return Math.floor(Math.random()*6) +1
     })
 }
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
 
-function getDiceHtml(diceCount){
-    return getDiceRollArray(diceCount).map(function(num){
-        return '<div class="dice">' + num + '</div>'
-    }).join("")
+function render(){
+    document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml()
+    document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml()
 }
 
-renderCharacter(hero)
-renderCharacter(monster)
+render()
