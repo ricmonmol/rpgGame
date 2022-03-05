d6dc7475429bc5d9d11521a1fc38c8d5479db47b
ricardo <rmontecinosmolina@gmail.com>
2022-03-05 17:45:05 -0300

    New files & monster, fixed if & =>

--- Character.js
+++ Character.js
@@ -1,25 +1,35 @@
-import {getDiceRollArray, getDicePlaceholderHtml} from './utils.js'
+import {getDiceRollArray, getDicePlaceholderHtml, getPercentage} from './utils.js'
+
 
 function Character(data){
     Object.assign(this,data)
+
+    this.maxHealth = this.health
+    
     this.diceArray = getDicePlaceholderHtml(this.diceCount)
+    
     this.getDiceHtml = function(){
         this.currentDiceScore = getDiceRollArray(this.diceCount)
-        this.diceArray = this.currentDiceScore.map(function(num){
-            return '<div class="dice">' + num + '</div>'
-        }).join("")
+        this.diceArray = this.currentDiceScore.map((num) => '<div class="dice">' + num + '</div>').join("")
     }
     this.getCharacterHtml = function(){
         const {elementId,name,avatar,health,diceCount,diceArray} = this
-        return '<div class="character-card"><h4 class="name">' + name + '</h4><img class="avatar" src=' + avatar + '><p class="health">health: <b>' + health + '</b></p><div class="dice-container">' + diceArray + '</div></div>'
+        const healthBar = this.getHealthBarHtml()
+        return '<div class="character-card"><h4 class="name">' + name + '</h4><img class="avatar" src=' + avatar + '><p class="health">health: <b>' + health + '</b></p>' + healthBar + '<div class="dice-container">' + diceArray + '</div></div>'
     }
 
     this.takeDamage = function(attackScoreArray){
-        const totalAttackScore = attackScoreArray.reduce(function(total, num){
-            return total + num
-        })
-
+        const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
         this.health -= totalAttackScore
+        if (this.health <= 0){
+            this.health = 0
+            this.dead = true
+        }
+    }
+
+    this.getHealthBarHtml = function(){
+        const percent = getPercentage(this.health, this.maxHealth)
+        return '<div class="health-bar-outer"><div class="health-bar-inner '+ (percent < 26 ? "danger" : "") +'" style="width:'+ percent +'%"></div></div>'
     }
 }
 
